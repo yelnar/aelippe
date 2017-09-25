5f2fc29209fdbbfe869f92007133484fd11d780a
Yelnar N <y.nauryzbaev@samsung.com>
2017-09-25 19:47:02 +0300

    Add share meta tags

---
+++ src/assets/scripts/share-text.js
@@ -0,0 +1,11 @@
+/**
+ * Set share buttons text
+ */
+$(document).ready(function ($) {
+  $('.rrssb-buttons').rrssb({
+    title: 'Aelippe',
+    url: 'http://aelippe.kz/',
+    description: 'Aelippe - Переводчик казахского текста с кириллической графики на латиницу',
+    emailBody: 'Aelippe - Переводчик казахского текста с кириллической графики на латиницу, aelippe.kz'
+  });
+});
