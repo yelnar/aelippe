a418a1a3f90c34e77d1c9c1c58e23299d23a00da
Yelnar <yeljnar@gmail.com>
2017-10-26 22:47:04 +0300

    Update alphabet

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -14,30 +14,24 @@ export class AppService {
 
   private getAlphabet(): Map<string, string> {
     const cyrillic = {
-      base: 'АаБбЦцДдЕеФфГгХхИиЙйКкЛлМмНнОоПпҚқРрСсТтҰұВвУуЫыІіЗз',
-      vowels: 'ӘәӨөҮү',
-      consonant: 'ҒғЧчШшЖж',
-      lower: ['ң', 'h'],
-      special: ['Ё', 'ё', 'Ц', 'ц', 'Щ', 'щ', 'Э', 'э', 'Ю', 'ю', 'Я', 'я', 'Ь', 'ь', 'Ъ', 'ъ']
+      base: 'АаБбДдЕеФфГгХхІіЖжКкЛлМмНнОоПпҚқРрСсТтҰұВвЫыЗз',
+      special: 'ӘәҒғИиЙйӨөШшЧчҮүУу',
+      lower: ['h', 'ң'],
+      other: ['Ё', 'ё', 'Ц', 'ц', 'Щ', 'щ', 'Э', 'э', 'Ю', 'ю', 'Я', 'я', 'Ь', 'ь', 'Ъ', 'ъ']
     };
 
     const latin = {
-      base: 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwYyIiZz',
-      vowels: 'AaOoUu',
-      consonant: 'GgCcSsZz',
-      lower: ['ng', 'h'],
-      special: ['Io', 'io', 'Ts', 'ts', 'Sh', 'sh', 'E', 'e', 'Iw', 'iw', 'Ia', 'ia', '', '', '', '']
+      base: 'AaBbDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvYyZz',
+      special: 'ÁáǴǵÍíÍíÓóŚśĆćÚúÝý',
+      lower: ['h', 'ń'],
+      other: ['Io', 'io', 'Ts', 'ts', 'Ś', 'ś', 'E', 'e', 'Iý', 'iw', 'Ia', 'ia', '', '', '', '']
     };
 
     const map = new Map();
 
     Object.keys(cyrillic).forEach((key) => {
-      let ending = '';
-      if (key === 'vowels') { ending = 'e'; }
-      if (key === 'consonant') { ending = 'h'; }
-
       for (let i = 0; i < cyrillic[key].length; i++) {
-        map.set(cyrillic[key][i], latin[key][i] + ending);
+        map.set(cyrillic[key][i], latin[key][i]);
       }
     });
 
